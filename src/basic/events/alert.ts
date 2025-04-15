f86a3cf98e094b0da9b353f3090225f73df286cb
mini--inim <forestaroundseoul@gmail.com>
2025-04-16 03:49:47 +0900

    fix: alert 수정

--- src/basic/events/alert.ts
+++ src/basic/events/alert.ts
@@ -1,28 +1,43 @@
-//할인 알람
+import { PRODUCTS } from "../context/product";
+import { updateStockOption } from "./stock";
+
+
+//깜짝 세일 알람
 function discountAlert () {
-    setInterval(function () {
-      var luckyItem=prodList[Math.floor(Math.random() * prodList.length)];
-      if(Math.random() < 0.3 && luckyItem.q > 0) {
-        luckyItem.val=Math.round(luckyItem.val * 0.8);
-        alert('번개세일! ' + luckyItem.name + '이(가) 20% 할인 중입니다!');
-        //updateSelOpts();
+  setInterval(function () {
+    const luckyItem = PRODUCTS[Math.floor(Math.random() * PRODUCTS.length)];
+
+    if(Math.random() < 0.3 && luckyItem.stock > 0) {
+      luckyItem.price = Math.round(luckyItem.price * 0.8);
+
+      alert(`번개세일! ${luckyItem.name}이(가) 20% 할인 중입니다!`);
+      
+      updateStockOption();
+    }
+  }, 30000);
+};
+
+
+//추천 세일 알람
+function recommandAlert() {
+
+  const itemList = document.querySelectorAll('#cart-items > div');
+  const lastSel = itemList[0].id;
+
+  setInterval(function () {
+    if(lastSel) {
+      const suggest = PRODUCTS.find(function (item) { 
+        return item.id !== lastSel && item.stock > 0; 
+      });
+
+      if(suggest) {
+        alert(`${suggest.name}은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!`);
+        suggest.price=Math.round(suggest.price * 0.95);
+        updateStockOption();
       }
-    }, 30000);
-  };
+    }
+  }, 60000);
+};
 
 
-  //추가 구매 제안 알람
-  function addProdcutAlert() {
-    setInterval(function () {
-      if(lastSel) {
-        var suggest=prodList.find(function (item) { return item.id !== lastSel && item.q > 0; });
-        if(suggest) {
-          alert(suggest.name + '은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!');
-          suggest.val=Math.round(suggest.val * 0.95);
-          //updateSelOpts();
-        }
-      }
-    }, 60000);
-  };
-
-  export {addProdcutAlert, discountAlert};
+export {recommandAlert, discountAlert};
