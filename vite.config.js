8ee034a6b6d704659fb95ac253fd897ee0129057
mini--inim <forestaroundseoul@gmail.com>
2025-04-18 06:13:10 +0900

    fix: vite.config 수정

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,35 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { resolve } from 'path'
 
-export default defineConfig({
-  base: '/front_5th_chapter2-1/',
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        basic: path.resolve(__dirname, 'index.html'),
-        advanced: path.resolve(__dirname, 'index.advanced.html'),
+export default defineConfig(({ mode }) => {
+  const base = mode === "production" ? "/front_5th_chapter2-1/" : "/";
+  return {
+    // 2. base URL 설정
+    base: base,
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.basic.html'),
+          advanced: resolve(__dirname, 'index.advanced.html'),
+        },
+      },
+      outDir: 'dist',
+    },
+
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: ['/src/setupTests.js'],
+      include: [
+        'src/basic/**/*.test.js',
+        'src/advanced/**/*.test.js',
+      ],
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
       },
     },
-  },
-  server: {
-    open: '/index.html',
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
-  },
+  }
 })
