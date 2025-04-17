9a5cd4263d355c584e0aa051656a9a683b762d2f
mini--inim <forestaroundseoul@gmail.com>
2025-04-18 06:07:51 +0900

    배포 준비

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  base: '/front_5th_chapter2-1/',
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        basic: path.resolve(__dirname, 'index.html'),
+        advanced: path.resolve(__dirname, 'index.advanced.html'),
+      },
+    },
+  },
+  server: {
+    open: '/index.html',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
