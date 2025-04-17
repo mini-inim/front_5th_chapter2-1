e55a67e3add8f4cd955819319cd76f2897f69c2a
mini--inim <forestaroundseoul@gmail.com>
2025-04-17 22:40:49 +0900

    fetch: React Component 분리

--- src/advanced/main.advanced.tsx
+++ src/advanced/main.advanced.tsx
@@ -2,7 +2,7 @@ import React from 'react'; // ✅ 추가
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('app')!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
